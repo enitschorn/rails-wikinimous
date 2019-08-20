af8f3a141b063f0ed89782d014243063395eb274
Christine Schorn <cschorn91@gmail.com>
2019-08-20 16:15:23 +1000

    created model and controller and first action#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning up DB'
+Article.destroy_all
+
+puts 'Creating fake articles...'
+10.times do
+  article = Article.new(
+    title:   Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  article.save!
+end
+puts "Finished creating #{Article.count} articles!"
